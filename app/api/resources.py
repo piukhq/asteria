c36c19135b4c1b9fb93102b6a8e60b152202b6ab
Francesco Milani <fmilani@bink.com>
2020-11-13 12:51:42 +0200

    correct query string parse

--- app/api/resources.py
+++ app/api/resources.py
@@ -1,12 +1,13 @@
 import falcon
 from prometheus_client import REGISTRY
 from prometheus_client.exposition import choose_encoder
+from urllib.parse import parse_qs
 
 
 class Metrics:
-    def on_get(self, req, resp):
+    def on_get(self, req: falcon.Request, resp: falcon.Response) -> None:
         registry = REGISTRY
-        params = req.params
+        params = parse_qs(req.query_string)
         encoder, resp.content_type = choose_encoder(req.accept)
         if "name[]" in params:
             registry = registry.restricted_registry(params["name[]"])
@@ -15,5 +16,5 @@ class Metrics:
 
 
 class Healthz:
-    def on_get(self, req, resp):
+    def on_get(self, req: falcon.Request, resp: falcon.Response) -> None:
         resp.status = falcon.HTTP_200
