ac2473e679fa8109d546dc39c46841f2efe0f39c
Francesco Milani <fmilani@bink.com>
2020-11-13 13:18:12 +0200

    add db exception handling

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,12 +4,19 @@ from prometheus_client import REGISTRY
 from app.api.resources import Healthz, Metrics
 from app.metrics import CustomCollector
 from settings import LOGGER
+from sqlalchemy.exc import DBAPIError
+
+
+def database_exception_handler(req, resp, ex, params):
+    LOGGER.exception("Database disconnected abnormally.")
+    raise falcon.HTTPError(falcon.HTTP_500, "Database disconnected abnormally.")
 
 
 def create_app():
     LOGGER.info("Registering metrics.")
     REGISTRY.register(CustomCollector())
-    app = falcon.API()
+    app = falcon.API(media_type="text/plain")
+    app.add_error_handler(DBAPIError, database_exception_handler)
     app.add_route("/metrics", Metrics())
     app.add_route("/healthz", Healthz())
     LOGGER.info("Initialise wsgi app.")
